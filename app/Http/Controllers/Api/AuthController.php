dca5dab06fbf5392e14dddc7a168f14933362fcc
sano <cedric-valcy@hotmail.fr>
2024-11-05 20:02:04 +0100

    Ajout de la methode register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -15,9 +17,9 @@ class AuthController extends Controller
     /**
      * Fonction pour que l'utilisateur puisse se connecter
      * 
-     * @return Response
+     * @return JsonResponse
      */
-    public function login(LoginRequest $request): Response
+    public function login(LoginRequest $request): JsonResponse
     {
 
         // Récupération des champs du login
@@ -25,19 +27,60 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             /**
-             *
              * @var  User
              */
             $user = Auth::user();
-            $token = $user->createToken('authToken', ['*'])->plainTextToken;
-            return response(
+            $token = $user->createToken('authToken', ['*'], now()->addDay())->plainTextToken;
+            var_dump(now()->addDay());
+            return response()->json(
                 [
+                    'message' => 'Success login',
                     'token' => $token
                 ],
                 200,
             );
+        } else {
+            return response()->json(
+                [
+                    'message' => 'Authenticated failed'
+                ],
+                401
+            );
         }
     }
+
+
+    /**
+     * Inscription de l'utilisateur
+     *
+     * @return Response
+     */
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        try {
+            $fields = $request->all();
+            $user = User::create([
+                'name' => $fields['name'],
+                'email' => $fields['email'],
+                'password' => $fields['password'],
+            ]);
+
+            if (!$user)
+                throw new Exception('Imposssible de créer un utilisateur', 401);
+
+            $token = $user->createToken('authToken', ['*'], now()->addDay())->plainTextToken;
+            return response()->json(
+                [
+                    'message' => 'Success login',
+                    'token' => $token
+                ],
+                201,
+            );
+        } catch (Exception $e) {
+            Log::error($e->getMessage(), 'error');
+        }
+    }
+
 
     /**
      * Remove the specified resource from storage.
